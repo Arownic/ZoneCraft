444c40cbfe41ac7cb7609f9a5caf1843fbd1a6f8
Arownic <rgapvp2rbx21@gmail.com>
2025-06-25 16:56:47 -0500

    Initial Commit

---
+++ src/main/java/zonecraftmod/ZoneCraftMod.java
@@ -0,0 +1,22 @@
+package zonecraftmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ZoneCraftMod.MODID)
+public class ZoneCraftMod {
+    public static final String MODID = "zonecraft";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public ZoneCraftMod() {
+        //IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+}
